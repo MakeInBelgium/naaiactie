f8d0023fb3d125b0c5ef701dd2466c371c944f3e
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2020-03-17 23:10:47 +0100

    Dev build deployment fixes

--- docker-run.sh
+++ docker-run.sh
@@ -15,7 +15,7 @@ fi
 
 FULL_DOMAIN="${SUBDOMAIN}.${DOMAIN_NAME}"
 
-docker build -t babbelbox:$BRANCH --build-arg vcsref=$(git rev-parse --short HEAD) --build-arg DOMAIN=$FULL_DOMAIN .
+docker build -t mondmasker:$BRANCH --build-arg vcsref=$(git rev-parse --short HEAD) --build-arg DOMAIN=$FULL_DOMAIN .
 docker stop $FULL_DOMAIN && docker rm $FULL_DOMAIN
 
 if [ $BRANCH == master ]
@@ -23,28 +23,28 @@ then
     docker run -d \
         --network proxy \
         -l traefik.enable=true \
-        -l traefik.http.routers.bb-$SUBDOMAIN.entrypoints=http \
-        -l traefik.http.routers.bb-$SUBDOMAIN.rule="Host(\`${DOMAIN_NAME}\`, \`www.${DOMAIN_NAME}\`)" \
-        -l traefik.http.routers.bb-$SUBDOMAIN.middlewares=https-redirect@file \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.entrypoints=https \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.rule="Host(\`${DOMAIN_NAME}\`, \`www.${DOMAIN_NAME}\`)" \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls=true \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls.certresolver=http \
+        -l traefik.http.routers.mm-$SUBDOMAIN.entrypoints=http \
+        -l traefik.http.routers.mm-$SUBDOMAIN.rule="Host(\`${DOMAIN_NAME}\`, \`www.${DOMAIN_NAME}\`)" \
+        -l traefik.http.routers.mm-$SUBDOMAIN.middlewares=https-redirect@file \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.entrypoints=https \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.rule="Host(\`${DOMAIN_NAME}\`, \`www.${DOMAIN_NAME}\`)" \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.tls=true \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.tls.certresolver=http \
         -l traefik.docker.network=proxy \
         --name "${FULL_DOMAIN}" \
-        babbelbox:$BRANCH
+        mondmasker:$BRANCH
 else
     docker run -d \
         --network proxy \
         -l traefik.enable=true \
-        -l traefik.http.routers.bb-$SUBDOMAIN.entrypoints=http \
-        -l traefik.http.routers.bb-$SUBDOMAIN.rule="Host(\`${FULL_DOMAIN}\`)" \
-        -l traefik.http.routers.bb-$SUBDOMAIN.middlewares=https-redirect@file \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.entrypoints=https \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.rule="Host(\`${FULL_DOMAIN}\`)" \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls=true \
-        -l traefik.http.routers.bb-$SUBDOMAIN-secure.tls.certresolver=http \
+        -l traefik.http.routers.mm-$SUBDOMAIN.entrypoints=http \
+        -l traefik.http.routers.mm-$SUBDOMAIN.rule="Host(\`${FULL_DOMAIN}\`)" \
+        -l traefik.http.routers.mm-$SUBDOMAIN.middlewares=https-redirect@file \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.entrypoints=https \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.rule="Host(\`${FULL_DOMAIN}\`)" \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.tls=true \
+        -l traefik.http.routers.mm-$SUBDOMAIN-secure.tls.certresolver=http \
         -l traefik.docker.network=proxy \
         --name "${FULL_DOMAIN}" \
-        babbelbox:$BRANCH
+        mondmasker:$BRANCH
 fi
