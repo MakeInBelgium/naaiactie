1adcc6f46a32662656e48a1d683895f470c539a7
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2020-03-19 15:00:16 +0100

    Clear cache

--- scripts/cache.py
+++ scripts/cache.py
@@ -1,15 +1,23 @@
 """
-# Check zones
+Very basic script to clear HTML files from the cache. On every push to master
+this script will clear the most commonly used HTML pages.
+
+If css or pdf's have changed we might need to Purge the cache. Needs to be
+done in Cloudflare's dashboard: https://dash.cloudflare.com/
+
+Get an overview of the available zones and their zone_id.
+
 curl -X GET "https://api.cloudflare.com/client/v4/zones" \
      -H "X-Auth-Email: tech@coronadenktank.be" \
      -H "X-Auth-Key: 9c56047583875706a14ad38f3280bf381c452" \
-     -H "Content-Type: application/json"
+     -H "Content-Type: application/json" | jq .
 """
 import json
 import os
 
+# Key value mapping of urls that needs to be cleard
+# - Key format: <naked domain>_ <cloudflare_zone_id>
 MAPPING = {
-    # maakjemondmasker.be
     'maakjemondmasker.be_797f4fdc62bcccad8192c6a085d55021': [
         'https://maakjemondmasker.be',
         'https://www.maakjemondmasker.be',
@@ -21,16 +29,18 @@ MAPPING = {
         'https://www.maakjemondmasker.be/disclaimer_nl.html',
     ],
 
-    # faitesvotremasquebuccal.be
     'faitesvotremasquebuccal.be_1396e8353ba83f820cec2d6df7e02869': [
         'https://faitesvotremasquebuccal.be',
         'https://faitesvotremasquebuccal.be/',
+        'https://faitesvotremasquebuccal.be/index.html',
+        'https://faitesvotremasquebuccal.be/disclaimer_fr.html',
     ],
 
-    # makefacemasks.com
     'makefacemasks.com_d2397b2fdf55f83933c9a227e6b7256b': [
-        'https://makefacemasks.com'
-        'https://makefacemasks.com/'
+        'https://makefacemasks.com',
+        'https://makefacemasks.com/',
+        'https://makefacemasks.com/index.html',
+        'https://makefacemasks.com/disclaimer_en.html',
     ]
 }
 
@@ -41,19 +51,14 @@ CONFIG_API_KEY = '9c56047583875706a14ad38f3280bf381c452'
 
 def clear(mapping):
     """
-    Example
-    -------
-
-    curl -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
-         -H "Authorization: Bearer IcxU3A5IVU4vrGs8-ioSqT-b3rpwfPKAL38njutq" \
-         -H "Content-Type:application/json"
+    Examples
+    --------
 
     curl -X POST "https://api.cloudflare.com/client/v4/zones/797f4fdc62bcccad8192c6a085d55021/purge_cache" \
          -H "X-Auth-Email: tech@coronadenktank.be" \
          -H "X-Auth-Key: 9c56047583875706a14ad38f3280bf381c452" \
          -H "Content-Type: application/json" \
          --data '{"files":["https://maakjemondmasker.be/css/test.css"]}'
-
     """
     for key, value in mapping.items():
         domain, zone_id = key.split('_')
